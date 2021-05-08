e2b698ba800c00424550fa96c976053cae35a6ba
Gabriel Ribeiro <49738156+Gab1235@users.noreply.github.com>
2021-05-08 15:54:59 -0400

    added async storage

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,11 +1,17 @@
 import React from "react";
 import { View, Text } from "react-native";
 import { useNavigation } from "@react-navigation/native";
+// import api from "../../services/api";
 
 import styles from "./styles";
 
 function Home() {
   const navigation = useNavigation();
+
+  // useEffect(() => {
+  //   api.get("home").then((response) => {
+  //   });
+  // }, []);
 
   return (
     <View style={styles.container}>
