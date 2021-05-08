0225d047bb8abc2e457b575fde51bf791f4e8b89
Gabriel Ribeiro <49738156+Gab1235@users.noreply.github.com>
2021-05-08 13:37:56 -0400

    added components

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,9 +1,12 @@
 import React from "react";
 import { View, Text } from "react-native";
+import { useNavigation } from "@react-navigation/native";
 
 import styles from "./styles";
 
 function Home() {
+  const navigation = useNavigation();
+
   return (
     <View style={styles.container}>
       <Text>Hello World</Text>
